11ac9ac9cb130e42ab905b29686c83e6389e7929
aaaa <yvan.nedelec@etu.univ-poitiers.fr>
2025-11-17 16:51:16 +0100

    feat(routes): ✨ add playlist detail page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,30 @@ import WelcomePage from './pages/WelcomePage/WelcomePage.jsx';
 import TopTracksPage from './pages/TopTracksPage/TopTracksPage.jsx';
 import TopArtistsPage from './pages/TopArtistsPage/TopArtistsPage.jsx';
 import PlaylistsPage from './pages/PlaylistsPage/PlaylistsPage.jsx';
+import PlaylistPage from './pages/PlaylistPage/PlaylistPage.jsx'; 
 import Callback from './pages/Callback.jsx';
 import Layout from './components/Layout/Layout.jsx';
 import LoginPage from './pages/LoginPage/LoginPage.jsx';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx';
 import AccountPage from './pages/AccountPage/AccountPage.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { index: true, element: <WelcomePage /> },
-      { path: 'login', element: <LoginPage /> },
-      { path: 'callback', element: <Callback /> },
-      { path: 'account', element: <AccountPage /> },
-      { path: 'top-tracks', element: <TopTracksPage /> },
-      { path: 'top-artists', element: <TopArtistsPage /> },
-      { path: 'playlists', element: <PlaylistsPage /> },
-      { path: '*', element: <NotFoundPage /> }
-    ]
-  }
-]);
-
-export function App() {
-  return <RouterProvider router={router} />;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<WelcomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="account" element={<AccountPage />} />
+          <Route path="playlists" element={<PlaylistsPage />} />
+          <Route path="playlist/:id" element={<PlaylistPage />} />
+          <Route path="top-artists" element={<TopArtistsPage />} />
+          <Route path="top-tracks" element={<TopTracksPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
